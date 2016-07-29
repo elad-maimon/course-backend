e01574d7e614ea228bdbab994936ab4f44efadcd
Elad Maimon <eladm@google.com>
2016-07-29 23:50:57 +0300

    UsersController: support follow and unfollow

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,12 +1,13 @@
 class API::UsersController < API::APIController
   skip_before_action :validate_authentication, only: :create
+  before_action :set_user, only: [:show, :follow, :unfollow]
 
   def index
     respond_with User.all, each_serializer: UserShortSerializer
   end
 
   def show
-    respond_with User.find params[:id]
+    respond_with @user
   end
 
   def create
@@ -15,14 +16,24 @@ class API::UsersController < API::APIController
   end
 
   def follow
-    render json: 'follow'
+    current_user.follow @user.id
+    respond_with @user, location: nil
+  rescue ActiveRecord::RecordNotUnique
+    raise ApiParameterException.new.add_error :id, 'already following'
   end
 
   def unfollow
-    render json: 'unfollow'
+    current_user.unfollow @user.id
+    respond_with @user, location: nil
+  rescue ActiveRecord::RecordNotFound
+    raise ApiParameterException.new.add_error :id, 'not following yet'
   end
 
   private
+
+  def set_user
+    @user = User.find params[:id]
+  end
 
   def user_params
     params.permit :name, :email, :password
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,6 +2,6 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :created_at
 
   has_many :posts
-  has_many :following, serializer: ShortUserSerializer
-  has_many :followers, serializer: ShortUserSerializer
+  has_many :following, serializer: UserShortSerializer
+  has_many :followers, serializer: UserShortSerializer
 end
